ecd027e8235a0b6884a2dc29bb2036de1cf7886c
smart8486 <smart8486@gmail.com>
2021-07-21 17:22:35 -0400

    Update HomeScreen.js

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -6,7 +6,7 @@ export default function HomeScreen() {
             <h1>Bem-Vindo</h1>
             
           <footer class="main-footer">
-          Projeto desenvolvido em React por @Wesley
+          Projeto desenvolvido em eactJS, React Router Dom para rotas e React Toastify @Wesley
         </footer>
 
         </div>
