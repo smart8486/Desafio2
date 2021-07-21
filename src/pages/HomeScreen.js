30ab90fe5d3620c7d548d3630ad5561fb32c8d48
smart8486 <smart8486@gmail.com>
2021-07-21 17:52:56 -0400

    Update HomeScreen.js

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function HomeScreen() {
     return (
-        <div className="container">
+        <div className="container" background-color >
             <h1>Bem-Vindo</h1>
             
           <footer class="main-footer">
-          Projeto desenvolvido em eactJS, React Router Dom para rotas e React Toastify @Wesley
+          Projeto desenvolvido em eactJS, React Router Dom para rotas e React Toastify <strong>@Wesley</strong>
         </footer>
 
         </div>
